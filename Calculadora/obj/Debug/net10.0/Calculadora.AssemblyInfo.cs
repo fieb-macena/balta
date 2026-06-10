285b68346f90cf37ee5f6164d8a662c9782cbbdb
victor.macena <=>
2026-06-10 16:50:14 -0300

    Editor de arquivos versão básica

--- Calculadora/obj/Debug/net10.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net10.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c5ec0a94dbcdd63a131f149873511ebf6c91e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3875580708abdaa4d2794a172d1ae88cd29ad10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TextEditor/Program.cs
@@ -0,0 +1,61 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.WriteLine("Digite a opção desejada:");
+    Console.WriteLine("1 - Abrir arquivo, 2 - Novo Arquivo, 0 - Sair");
+    string inputMenu = Console.ReadLine();
+    switch (inputMenu)
+    {
+        case "1": { Abrir(); break; }
+        case "2": { NovoArquivo(); break; }
+        case "0": { System.Environment.Exit(0); break; }
+        default: { Console.WriteLine("Opção inválida, tente novamente"); Menu(); break; }
+    }
+}
+
+static void NovoArquivo()
+{
+    Console.WriteLine("Edite o texto: (Pressione ESC para sair)");
+    var texto = "";
+
+    do
+    {
+        texto += Console.ReadLine();
+        texto += Environment.NewLine;
+    }while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Console.WriteLine("Deseja salvar o arquivo? (S/N)");
+    if(Console.ReadLine().ToUpper() == "S")
+        Salvar(texto);
+    else
+        System.Environment.Exit(0);
+
+}
+
+static void Salvar(string text)
+{
+    Console.Clear();
+    Console.WriteLine("Digite o caminho do arquivo a ser salvo:");
+    string caminho = Console.ReadLine();
+
+    using (var arquivo = new StreamWriter(caminho))
+    {
+        arquivo.Write(text);
+    }
+
+    Console.WriteLine("Arquivo salvo com sucesso em " + caminho);
+}
+
+static void Abrir()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o caminho do arquivo a ser editado:");
+    string caminho = Console.ReadLine();
+
+    using (var arquivo = new StreamReader(caminho))
+    {
+        string texto = arquivo.ReadToEnd();
+        Console.WriteLine(texto);
+    }
+}
